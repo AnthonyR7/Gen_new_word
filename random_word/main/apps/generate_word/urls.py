ae0d740c6f5a57e64f52d0d4b6979bc5ffd1b7e4
AnthonyR7 <34079786+AnthonyR7@users.noreply.github.com>
2018-05-15 12:36:43 -0700

    Update urls.py

--- random_word/main/apps/generate_word/urls.py
+++ random_word/main/apps/generate_word/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^generate$', views.generate_a_word),
-    # url(r'^reset$', views.reset)
+    url(r'^reset$', views.reset)
   ]
